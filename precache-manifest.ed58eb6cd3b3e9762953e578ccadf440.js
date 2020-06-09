04cd9f09a4d1ad51c1ef5ae77ca01cce5603843f
Parag Dangriya <45040382+ParagsDangariya@users.noreply.github.com>
2020-06-09 14:32:23 -0400

    Updates

---
+++ precache-manifest.ed58eb6cd3b3e9762953e578ccadf440.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ab11baeff279e53591e5504e143a4a",
+    "url": "/Parag_demo/index.html"
+  },
+  {
+    "revision": "7837ffd6f471165b78d4",
+    "url": "/Parag_demo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "db80d21c904c100fce40",
+    "url": "/Parag_demo/static/css/main.6e3b1212.chunk.css"
+  },
+  {
+    "revision": "7837ffd6f471165b78d4",
+    "url": "/Parag_demo/static/js/2.67a03bd2.chunk.js"
+  },
+  {
+    "revision": "b45be4dfcf889ea1b7385f58d15dba2d",
+    "url": "/Parag_demo/static/js/2.67a03bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db80d21c904c100fce40",
+    "url": "/Parag_demo/static/js/main.9d660f11.chunk.js"
+  },
+  {
+    "revision": "b289959a8382bf369793",
+    "url": "/Parag_demo/static/js/runtime-main.91c36c8a.js"
+  }
+]);
